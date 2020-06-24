99d2b0e992e57a3e7ba7f9918c68962b3937fb2c
sigmade <sychyov1991@mail.ru>
2020-06-24 22:34:09 +0300

    Add project files.

---
+++ EF console/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EF_console
+{
+    class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=EFconsole; Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EF console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace EF_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EF Lesson");
+            /*using (AppContext context = new AppContext())
+            {
+                context.Users.Add(new User() { Name = "Alex", Age = 28 });
+                context.SaveChanges();
+            }*/
+            using (AppContext context = new AppContext())
+            {
+                var list = context.Users.ToList();
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"{item.Id}-{item.Name}-{item.Age}");
+                }
+            }
+        }
+    }
+}
